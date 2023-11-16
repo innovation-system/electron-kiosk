580cb97f3611ba0f2d1ae1590bf69376e8d54289
Daniel Lando <daniel.sorridi@gmail.com>
2023-11-16 11:28:57 +0100

    chore: fix install script

--- install.sh
+++ install.sh
@@ -1,12 +1,25 @@
 echo '# Installing Electron KIOSK... '
 cd ~
-curl -s https://api.github.com/repos/innovation-system/electron-kiosk/releases/latest \
-| grep "browser_download_url.*AppImage" \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget -i -
+# Get arch
 
-FILE=$(ls | grep Electron-Kiosk*.AppImage)
+getUrl() {
+	ARCH=$(uname -m)
+	if [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
+		grep "browser_download_url.*-arm64.AppImage"
+	else
+		grep "browser_download_url.*.AppImage" \
+		| grep -v "browser_download_url.*-arm64.AppImage" 
+	fi
+}
+
+curl -s https://api.github.com/repos/innovation-system/electron-kiosk/releases/latest \
+	| getUrl \
+	| cut -d : -f 2,3 \
+	| tr -d \" \
+	| wget -i -
+
+
+FILE=$(ls | grep electron-kiosk*.AppImage)
 # Create .desktop on ~/.config/autostart
 mkdir -p ~/.config/autostart
 echo "[Desktop Entry]
